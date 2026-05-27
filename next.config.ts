7a3ec201f5b1b0d58a0a5c341e64c7090011d6c1
sureshdeveloperwebnox <sureshdeveloperwebnox@gmail.com>
2026-05-27 22:38:43 +0530

    feat: add BACKEND_API_URL environment variable to Dockerfile and implement dynamic rewrite destination in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
+    const backendUrl = process.env.BACKEND_API_URL || 'http://localhost:4000/api/v1/:path*';
     return [
       {
         source: '/api/v1/:path*',
-        destination: 'http://localhost:4000/api/v1/:path*',
+        destination: backendUrl,
       },
     ];
   },
